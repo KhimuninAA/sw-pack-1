f5ab890e880c24ad88baecf63a64b3d557ae50a6
Alexey <khimuninaa@gmail.com>
2022-10-17 14:54:34 +0300

    1.2.13

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         .library(
             name: "EDNAPushXCE",
             targets: ["EDNAPushXCE"]),
-        .library(name: "EDNAPushXCEStatic", type: .static, targets: ["EDNAPushXCE"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "EDNAPushXCE",
-            url: "https://github.com/KhimuninAA/sw-pack-1/releases/download/1.2.12/edna-push-xce-1.2.6.zip",
+            url: "https://github.com/KhimuninAA/sw-pack-1/releases/download/1.2.13/edna-push-xce-1.2.5-191b.zip",
             checksum: "0a431a11abc308cdbfe467bc86a6865942a5dd72fba9023eea6b04366825d89e"
         ),
     ]
